73386b47c0f496ccf6814b527a1577176c6830b3
DESKTOP-EITU1SO\eli52 <eli52583@hotmail.com>
2023-04-23 20:44:21 +0800

    chapter5: users

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider } from "react-router-dom";
 import AddPostForm from "./features/posts/AddPostForm";
 import PostsList from "./features/posts/PostsList";
 import SinglePost from "./features/posts/SinglePost";
 import EditPostForm from "./features/posts/EditPostForm";
+import UsersList from "./features/users/UsersList";
+import UserPage from "./features/users/UserPage";
 import Layout from "./components/Layout";
 
 const router = createBrowserRouter(
@@ -17,6 +19,13 @@ const router = createBrowserRouter(
       <Route path="edit/:postId" element={<EditPostForm />} />
     </Route>
 
+    <Route path="user">
+      <Route index element={<UsersList />} />
+      <Route path=":userId" element={<UserPage />} />
+    </Route>
+
+    {/* replace with 404 component */}
+    <Route path="*" element={<Navigate to="/" replace />}></Route>
   </Route>
   )
 )
